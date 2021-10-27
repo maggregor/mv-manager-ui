17881923ead185b65b5a9132787f2ec753142dba
Esteban Gonzalez <esteban@XPS-13-7390-2-in-1-2aa769d7.localdomain>
2021-10-27 17:06:46 +0200

    refacto skeleton usage

--- src/views/project/index.vue
+++ src/views/project/index.vue
@@ -2,9 +2,10 @@
   <div :class="$style.container">
     <div class="pl-4 d-flex flex-wrap align-items-center">
       <div class="mr-auto">
-        <div class="text-weight-600 text-gray-7 font-size-30">{{ project.projectName }}</div>
-        <div class="text-weight-200 text-gray-7 font-size-18">{{ project.projectId }}</div>
-        <!-- {{ datasets }} -->
+        <div v-if="project">
+          <div class="text-weight-600 text-gray-7 font-size-30">{{ project.projectName }}</div>
+          <div class="text-weight-200 text-gray-7 font-size-18">{{ project.projectId }}</div>
+        </div>
         <div class="mt-5">
           <div v-for="dataset in datasets" :key="dataset.datasetName">
             <a :to="`/${projectId}/overview`" class="m-1 btn btn-primary width-300">
@@ -20,7 +21,7 @@
 <script>
 import { computed, onMounted } from 'vue'
 import { useStore } from 'vuex'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 export default {
   name: 'Overview',
@@ -28,10 +29,12 @@ export default {
   setup() {
     const store = useStore()
     const route = useRoute()
+    const router = useRouter()
     const datasets = computed(() => store.getters['datasets/datasets'])
     const projectId = route.params.projectId
     onMounted(() => {
       store.dispatch('datasets/LOAD_DATASETS', { projectId: projectId })
+      .catch(e => router.push('/'))
     })
     const project = computed(() => store.getters['projects/getProjectById'](projectId))
     return {
