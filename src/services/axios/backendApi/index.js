3f241e350f1c4f4e33eadb0e509039e706286209
Esteban <esteban@Estebans-MacBook-Pro.local>
2021-11-05 09:48:00 +0100

    add api get query statics

--- src/services/axios/backendApi/index.js
+++ src/services/axios/backendApi/index.js
@@ -10,3 +10,10 @@ export async function getDatasets(payload) {
   const {data} = await apiClient.get(`/project/${projectId}/dataset`)
   return data
 }
+
+export async function getQueryStatistics(payload) {
+  let projectId = payload.projectId
+  let lastDays = payload.lastDays
+  const {data} = await apiClient.get(`/project/${projectId}/queries/${lastDays}/statistics`)
+  return data
+}
