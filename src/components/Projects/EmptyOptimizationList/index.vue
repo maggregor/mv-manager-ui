c3c81dc4a7f91b6d015757d72563a861133de741
estebgonza <esteban.gonzalez@achilio.com>
2022-03-10 21:01:51 +0100

    Wording

--- src/components/Projects/EmptyOptimizationList/index.vue
+++ src/components/Projects/EmptyOptimizationList/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>No optimizations triggered</h1>
+    <h1>No optimization</h1>
   </div>
 </template>
 <script>
--- src/components/Projects/NoLastOptimization/index.vue
+++ src/components/Projects/NoLastOptimization/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>Active some datasets, and start a first optimization</h1>
+    <h1>No optimization</h1>
   </div>
 </template>
 <script>
