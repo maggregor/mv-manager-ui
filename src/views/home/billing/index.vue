103eb89275cd0a821a0d42594878bd32976e3392
estebgonza <esteban.gonzalez@achilio.com>
2022-04-02 14:13:38 +0200

    switch monthly and yearly

--- src/views/home/billing/index.vue
+++ src/views/home/billing/index.vue
@@ -1,38 +1,62 @@
 <template>
-  <a-row type="flex" justify="center">
-    <a-col v-for="p in allProducts" :key="p.id" :span="10">
-      <PricingTable
-        :name="p.name"
-        :url-image="p.images[0]"
-        :prices="getPricesOfProduct(p.id)"
-        :description="p.description"
-        :includes="getIncludesOfProduct(p.id)"
-      />
-    </a-col>
+  <div class="billing">
+    <h3>Subscription</h3>
+    <a-row type="flex" class="mb-4" justify="center">
+      <a-button-group>
+        <a-button :type="isIntervalEquals('month') ? 'primary' : ''" @click="interval = 'month'">
+          Monthly
+        </a-button>
+        <a-button :type="isIntervalEquals('year') ? 'primary' : ''" @click="interval = 'year'">
+          Yearly
+        </a-button>
+      </a-button-group>
+    </a-row>
+    <a-row type="flex" justify="center">
+      <a-col v-for="p in allProducts" :key="p.id" :span="11">
+        <PricingTable
+          :name="p.name"
+          :url-image="p.images[0]"
+          :prices="getPricesOfProduct(p.id)"
+          :description="p.description"
+          :includes="getIncludesOfProduct(p.id)"
+          :interval="interval"
+        />
+      </a-col>
 
-    <a-col :span="10">
-      <PricingTable
-        contact
-        name="> 5 projects"
-        url-image="https://files.stripe.com/links/MDB8YWNjdF8xSThvTDFLejNUVjhYQmJkfGZsX3Rlc3RfMzNGSGtjVFVtTklYcng0MU5JTXZpbXo40087wHsIVd"
-        description="We are gonna to give the best sizing"
-        :includes="[
-          'Flexible pricing',
-          'Automatic generation',
-          'Custom SAML support',
-          'Dedicated slack channel',
-        ]"
-      />
-    </a-col>
-  </a-row>
+      <a-col :span="1"> </a-col>
+      <a-col :span="10">
+        <PricingTable
+          contact
+          name="> 5 projects"
+          url-image="https://files.stripe.com/links/MDB8YWNjdF8xSThvTDFLejNUVjhYQmJkfGZsX3Rlc3RfMzNGSGtjVFVtTklYcng0MU5JTXZpbXo40087wHsIVd"
+          description="We are gonna to give the best sizing"
+          :includes="[
+            'Flexible pricing',
+            'Automatic generation',
+            'Custom SAML support',
+            'Dedicated slack channel',
+          ]"
+        />
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import PricingTable from '@/components/PricingTable'
+import { ref } from '@vue/reactivity'
 export default {
   components: {
     PricingTable,
+  },
+  setup() {
+    const interval = ref('month')
+    const isIntervalEquals = i => interval.value === i
+    return {
+      interval,
+      isIntervalEquals,
+    }
   },
   computed: {
     ...mapGetters([
@@ -48,4 +72,7 @@ export default {
 
 <style lang="scss" scoped>
 @import './style.module.scss';
+a-switch {
+  font-size: 28px;
+}
 </style>
