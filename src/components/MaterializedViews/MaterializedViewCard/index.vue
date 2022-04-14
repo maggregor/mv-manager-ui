7ff2e76f2a1596f18911ebbf678c2d8b95e09ba7
estebgonza <esteban.gonzalez@achilio.com>
2022-04-14 11:45:17 +0200

    add timeout 1.5s

--- src/components/MaterializedViews/MaterializedViewCard/index.vue
+++ src/components/MaterializedViews/MaterializedViewCard/index.vue
@@ -18,7 +18,7 @@
             <right-outlined v-else />
           </div>
         </a-col>
-        <a-col class="p-1" :span="14">
+        <a-col class="p-1" :span="13">
           <a-tag
             v-if="mv.status === 'NOT_APPLIED' && mv.lastJobId === mv.initialJobId"
             color="green"
@@ -27,17 +27,17 @@
           <a> {{ mv.datasetName }}/{{ mv.tableName }}/{{ mv.mvDisplayName }} </a>
           <p>
             <i>Found by Achilio {{ moment(mv.createdAt).fromNow() }}</i
-            ><a-tag class="ml-3" color="red" v-if="mv.statusReason === 'ERROR_DURING_CREATION'"
-              >Error during creation</a-tag
-            >
+            ><a-tag class="ml-3" color="red" v-if="displayStatusReason">{{
+              displayStatusReason
+            }}</a-tag>
           </p>
         </a-col>
-        <a-col class="actions-container" :span="9">
+        <a-col class="actions-container" :span="10">
           <a-button v-if="loading" :loading="true" type="link">Processing...</a-button>
 
           <div v-else-if="mv.status === 'OUTDATED'">
             <a-tag style="height: 20px" color="orange">Outdated</a-tag>
-            <a-button @click="action('UNAPPLY_MATERIALIZED_VIEW')" type="link">Trash</a-button>
+            <a-button @click="action('UNAPPLY_MATERIALIZED_VIEW')" type="link">Remove</a-button>
           </div>
 
           <div v-else-if="mv.status === 'NOT_APPLIED'">
@@ -48,7 +48,15 @@
           </div>
 
           <div v-else-if="mv.status === 'APPLIED'">
-            <a-button @click="action('UNAPPLY_MATERIALIZED_VIEW')" type="link">Unapply</a-button>
+            <a-button @click="action('UNAPPLY_MATERIALIZED_VIEW')" type="link">Remove</a-button>
+          </div>
+          <div v-else-if="mv.status === 'UNKNOWN'">
+            <a-button @click="action('DISCARD_MATERIALIZED_VIEW')" type="link">
+              Discard this proposal
+            </a-button>
+            <a-button @click="action('UNAPPLY_MATERIALIZED_VIEW')" type="link"
+              >Try removing</a-button
+            >
           </div>
         </a-col>
       </a-row>
@@ -62,6 +70,7 @@
 import { DownOutlined, RightOutlined } from '@ant-design/icons-vue'
 import { ref } from '@vue/reactivity'
 import { useStore } from 'vuex'
+import { computed } from '@vue/runtime-core'
 const moment = require('moment')
 export default {
   name: 'MaterializedViewCard',
@@ -84,18 +93,33 @@ export default {
     const store = useStore()
     const projectId = store.getters['selectedProjectId']
     const loading = ref(false)
+    const statusDisplayable = {
+      ERROR_DURING_DELETION: {
+        message: 'Error during deletion',
+      },
+      ERROR_DURING_CREATION: {
+        message: 'Error during creation',
+      },
+    }
+    const displayStatusReason = computed(() => {
+      const mv = props.mv
+      if (statusDisplayable.hasOwnProperty(mv.statusReason)) {
+        return statusDisplayable[mv.statusReason].message
+      }
+    })
     const action = async actionName => {
       loading.value = true
       try {
         await store.dispatch(actionName, { projectId, id: props.mv.id })
       } catch (ignored) {}
-      loading.value = false
+      setTimeout(() => (loading.value = false), 1500)
     }
     return {
       opened,
       action,
       loading,
       moment,
+      displayStatusReason,
     }
   },
 }
