988aad123e6c7f3a9a3664d8eeb4cb398637ee81
estebgonza <esteban.gonzalez@achilio.com>
2022-05-14 17:16:03 +0200

    add type and query count

--- src/components/TableList/index.vue
+++ src/components/TableList/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <a-table :columns="columns" :data-source="allTables" :pagination="{ hideOnSinglePage: true }">
+  <a-table
+    :columns="columns"
+    :data-source="allTables"
+    :pagination="{ hideOnSinglePage: true, position: ['topRight'] }"
+  >
     <template #bodyCell="{ column, record }">
       <template v-if="column.key === 'dataset'">
         <a>
@@ -9,6 +13,16 @@
       <template v-else-if="column.key === 'table'">
         <a>
           {{ record.tableName }}
+        </a>
+      </template>
+      <template v-else-if="column.key === 'type'">
+        <a-tag color="blue">
+          {{ record.type }}
+        </a-tag>
+      </template>
+      <template v-else-if="column.key === 'query_count'">
+        <a>
+          {{ record.queryCount }}
         </a>
       </template>
       <template v-else-if="column.key === 'cost'">
@@ -32,9 +46,45 @@ const columns = [
     key: 'table',
   },
   {
-    title: 'Monthly cost',
+    title: 'Type',
+    dataIndex: 'type',
+    key: 'type',
+    filters: [
+      {
+        text: 'Materialized View',
+        value: 'MATERIALIZED_VIEW',
+      },
+      {
+        text: 'View',
+        value: 'VIEW',
+      },
+      {
+        text: 'Table',
+        value: 'TABLE',
+      },
+      {
+        text: 'External Table',
+        value: 'EXTERNAL_TABLE',
+      },
+    ],
+    onFilter: (value, record) => record.type.indexOf(value) === 0,
+  },
+  {
+    title: 'Query Count',
+    dataIndex: 'query_count',
+    key: 'query_count',
+    sorter: (a, b) => {
+      return a.queryCount - b.queryCount
+    },
+  },
+  {
+    title: 'Monthly cost (storage)',
     dataIndex: 'cost',
     key: 'cost',
+    sorter: (a, b) => {
+      return a.cost - b.cost
+    },
+    defaultSortOrder: 'descend',
   },
 ]
 
